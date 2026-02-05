c18aedb5c1ea97b276a42c5091f298bc2212b95b
albert <albert.greinoecker@gmail.com>
2026-02-05 08:35:11 +0100

    break und continue

--- src/VerschachtelteSchleifen.java
+++ src/VerschachtelteSchleifen.java
@@ -1,4 +1,30 @@
 public class VerschachtelteSchleifen {
+    static void koordinaten(int grenze)
+    {
+
+        for (int i = 1; i <= grenze ; i++) {
+            for (int j = 1; j <= grenze; j++) {
+                System.out.printf("(%2d,%2d)", i, j);
+            }
+            System.out.println();
+        }
+    }
+
+    void testBreak()
+    {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5)
+            {
+                break;
+            }
+            if (i == 3)
+            {
+                continue; //gehe in den nächsten Schleifendurchlauf
+            }
+        }
+        //Break bricht die Schleife einfach ab und wir befinden uns an dieser Position
+    }
+
     static void einmaleins(int grenze)
     {
         for (int i = 1; i <= grenze ; i++) {
@@ -9,6 +35,7 @@ public class VerschachtelteSchleifen {
         }
     }
     public static void main(String[] args) {
-        einmaleins(20);
+        //einmaleins(20);
+        koordinaten(10);
     }
 }
