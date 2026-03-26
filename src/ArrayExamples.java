4eaef960bb0c6f1ebc6f50851151b97f1a1bfdf4
albert <albert.greinoecker@gmail.com>
2026-03-26 10:21:23 +0100

    add example 6

--- src/ArrayExamples.java
+++ src/ArrayExamples.java
@@ -14,15 +14,34 @@ public class ArrayExamples
         return sum;
     }
 
-    public static void main(String[] args) {
-        int[] xx = new int[3];
-        xx[0] = 10;
-        xx[1] = 20;
-        xx[2] = 30;
-        ausgabe(xx);
-        int[] xx2 = new int[2];
-        ausgabe(xx2);
+    static int[] geradeMinusEins(int[] a)
+    {
+        int[] a2 = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0)  //wenn gerade
+            {
+                a2[i] = -1;
+            } else
+            {
+                a2[i] = a[i];
+            }
+        }
+        return a2;
+    }
 
-        int s = sum(xx);
+    public static void main(String[] args) {
+//        int[] xx = new int[3];
+//        xx[0] = 10;
+//        xx[1] = 20;
+//        xx[2] = 30;
+//        ausgabe(xx);
+//        int[] xx2 = new int[2];
+//        ausgabe(xx2);
+//
+//        int s = sum(xx);
+        //         0 1 2 3 4  5  6
+        int[] a = {1,6,4,8,12,13,14};
+        int[] a2 = geradeMinusEins(a);
+        ausgabe(a2);
     }
 }
