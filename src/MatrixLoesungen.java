481c3659ee6c5a07a811f125d77c69cde8fdec33
albert <albert.greinoecker@gmail.com>
2026-05-07 08:57:17 +0200

    solutions 8,9

--- src/MatrixLoesungen.java
+++ src/MatrixLoesungen.java
@@ -61,18 +61,57 @@ public class MatrixLoesungen {
 //        return z;
         return mat[zeilenNr];
     }
+
+    static boolean gewonnen(char[][] spielfeld)
+    {
+        for (int i = 0; i < spielfeld.length; i++) {
+            String zeile = "";
+            for (int j = 0; j < spielfeld[i].length; j++) {
+                zeile += spielfeld[i][j];
+            }
+            if (zeile.contains("OOOO") || zeile.contains("XXXX")) return true;
+        }
+        return false;
+    }
+
+    static boolean vergleich(int[][] mat1, int[][] mat2)
+    {
+        if (mat1.length != mat2.length) return false;
+
+        for (int i = 0; i < mat1.length; i++) {
+            if (mat1[i].length != mat2[i].length) return false;
+            for (int j = 0; j < mat1[i].length; j++) {
+                if (mat1[i][j] != mat2[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        int[][] mat = { {1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        aufgabe01(mat);
-        ausgabe(mat);
-        int[][] matZZ = zzMatrix(10,10);
-        System.out.println("Die Zufallszahlen-Matrix:");
-        ausgabe(matZZ);
-        int cnt = anzahl0en(matZZ);
-        System.out.println("Anzahl 0en: " + cnt);
-        int summe = summe(matZZ);
-        System.out.println("summe der Werte: " + summe);
-        int[] z = zeile(matZZ, 2);
-        System.out.println(Arrays.toString(z));
+//        int[][] mat = { {1,2,3,4},{5,6,7,8},{9,10,11,12}};
+//        aufgabe01(mat);
+//        ausgabe(mat);
+//        int[][] matZZ = zzMatrix(10,10);
+//        System.out.println("Die Zufallszahlen-Matrix:");
+//        ausgabe(matZZ);
+//        int cnt = anzahl0en(matZZ);
+//        System.out.println("Anzahl 0en: " + cnt);
+//        int summe = summe(matZZ);
+//        System.out.println("summe der Werte: " + summe);
+//        int[] z = zeile(matZZ, 2);
+//        System.out.println(Arrays.toString(z));
+
+        char[][] spielfeld = {
+                {'.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', 'X', '.', '.', '.'},
+                {'.', '.', 'O', 'X', '.', '.', '.'},
+                {'.', 'O', 'O', 'O', 'X', '.', '.'},
+                {'O', 'X', 'X', 'X', 'X', 'O', '.'}
+        };
+
+        boolean g = gewonnen(spielfeld);
+        System.out.println("Gewonnen:" + g);
+
     }
 }
